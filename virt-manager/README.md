1a3e7620b9e109fec833eb7828903d37702e5a3f
joshfng <me@joshfrye.dev>
2024-11-05 19:09:52 -0500

    virt-manager

---
+++ virt-manager/README.md
@@ -0,0 +1,11 @@
+# Virtual Machine Manager
+
+[docker hub](https://hub.docker.com/r/joshfng/virt-manager) | [github](https://github.com/joshfng/docker-images/tree/main/virt-manager)
+
+## Building
+
+```shell
+docker build -t joshfng/virt-manager:latest .
+
+docker push joshfng/virt-manager --all-tags
+```
---
+++ virt-manager/startapp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOSTS=${HOSTS:-}
+
+# Run the broadwayd daemon and point nginx to it
+/usr/local/bin/start
+
+# Note: No X server or wayland support--only cli and gtk3
+dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "${HOSTS}"
+dbus-launch gsettings set org.virt-manager.virt-manager.connections autoconnect "${HOSTS}"
+dbus-launch gsettings set org.virt-manager.virt-manager xmleditor-enabled true
+
+tmux send-keys -t ttyd dbus-launch\ virt-manager\ --no-fork Enter
+
+trap 'exit 0' SIGTERM
+while true; do sleep 1; done
